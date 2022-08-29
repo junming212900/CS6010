c257c9817613c4a076f06f57dc2176387c2e0128
junming212900 <junming.work@gmail.com>
2022-08-29 08:42:07 -0600

    update

--- Day5/String Analyzer/String Analyzer/main.cpp
+++ Day5/String Analyzer/String Analyzer/main.cpp
@@ -112,29 +112,30 @@ double AverageVowelsPerWord(string s){
 }
 
 int main() {
-   
-    cout<<"Enter a string containing one or more sentences:\n";
     string s;
+    cout<<"Enter a string containing one or more sentences:\n";
     getline(cin,s);
-//    while (s!="done"||s!="Done"){
-//        if (s =="done"||s=="Done"){
-//            cout <<"good bye\n";
-//        }
-//        else {
-    int NOW = NumWords(s);
-    int NOS = NumSentences(s);
-    int NOV = NumVowels(s);
-    int NOC = NumConsonants(s);
-    double AOW = AverageWordLength(s);
-    double AOV = AverageVowelsPerWord(s);
-    cout<< "Analysis:\n";
-    cout<< "Number of words:"<<NOW<<"\n";
-    cout<< "Number of sentences:"<<NOS<<"\n";
-    cout<< "Number of vowels:"<<NOV<<"\n";
-    cout<< "Number of consonants:"<<NOC<<"\n";
-    cout<< "Reading level (average word length):"<< AOW<<"\n";
-    cout<< "Average vowels per word:"<<AOV<<"\n";
+
+    while (s!="done"&&s!="Done"){
     
+        int NOW = NumWords(s);
+        int NOS = NumSentences(s);
+        int NOV = NumVowels(s);
+        int NOC = NumConsonants(s);
+        double AOW = AverageWordLength(s);
+        double AOV = AverageVowelsPerWord(s);
+        cout<< "Analysis:\n";
+        cout<< "Number of words:"<<NOW<<"\n";
+        cout<< "Number of sentences:"<<NOS<<"\n";
+        cout<< "Number of vowels:"<<NOV<<"\n";
+        cout<< "Number of consonants:"<<NOC<<"\n";
+        cout<< "Reading level (average word length):"<< AOW<<"\n";
+        cout<< "Average vowels per word:"<<AOV<<"\n";
+        cout<<"Enter a string containing one or more sentences:\n";
+        getline(cin,s);
+    }
+    
+    cout <<"good bye\n";
     
     
     return 0;
