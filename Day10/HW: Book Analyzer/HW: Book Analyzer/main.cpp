13735760b7d06d897ecae5004de5be63e246c79a
junming212900 <junming.work@gmail.com>
2022-09-12 22:33:06 -0600

    Group HW: Bit Manipulation

--- Day10/HW: Book Analyzer/HW: Book Analyzer/main.cpp
+++ Day10/HW: Book Analyzer/HW: Book Analyzer/main.cpp
@@ -12,29 +12,29 @@
 
 using namespace std;
 
-string findTitle(vector<string> words) {
-    string title;
-    int start, end;
+string findTitle(vector<string> words) { // set up the string in check in the vector
+    string title; // create string variable  "title"
+    int start, end;// create the two variable "start" and "end "
     for (int i = 0; i < words.size(); i++) {
-        if (words[i] == "Title:") {
-            start = i;
+        if (words[i] == "Title:") { // scan the file to one by one to find the word"title";
+            start = i; // find the location of the first index  the "title" = start
             break;
         }
     }
     for (int i = 0; i < words.size(); i++) {
-        if (words[i] == "Author:") {
-            end = i;
+        if (words[i] == "Author:") { //scan the file to one by one to find the word"Author ";
+            end = i; // find the location last index   = end
             break;
         }
     }
     for (int i = start + 1; i < end; i++) {
         title += words[i];
-        title += " ";
+        title += " "; // print out the title name from start to end
     }
     return title;
 }
 
-string findAuthor(vector<string> words) {
+string findAuthor(vector<string> words) { // same above
     string title;
     int start, end;
     for (int i = 0; i < words.size(); i++) {
@@ -56,7 +56,7 @@ string findAuthor(vector<string> words) {
     return title;
 }
 
-string findReleaseDate(vector<string> words) {
+string findReleaseDate(vector<string> words) { // same above
     string title;
     int start, end;
     for (int i = 0; i < words.size(); i++) {
@@ -78,7 +78,7 @@ string findReleaseDate(vector<string> words) {
     return title;
 }
 
-int totalChar(vector<string> words) {
+int totalChar(vector<string> words) { // calculat the total word
     int sum = 0;
     for (auto word : words) {
         sum += word.size();
@@ -86,10 +86,10 @@ int totalChar(vector<string> words) {
     return sum;
 }
 
-string shortestWord(vector<string> words) {
+string shortestWord(vector<string> words) { // set up "a" shortest word and find out
     string shortest = "a";
-    for (auto word : words) {
-        if (word.size() <= shortest.size()) {
+    for (auto word : words) {// auto is automatily identify the type of the variable in the for loop
+        if (word.size() <= shortest.size()) { //compare the shortest word to next word. if the next word is shorter than the current word then the current word will replacce the periously word.
             return word;
         }
     }
@@ -99,26 +99,26 @@ string shortestWord(vector<string> words) {
 string longestWord(vector<string> words) {
     string result;
     for (auto word : words) {
-        if (word.size() >= result.size()) {
+        if (word.size() >= result.size()) {// compare the longest word to next word. if the next word is longer than the current word then the current word will replacce the periously word.
             result = word;
         }
     }
     return result;
 }
 
-void keyWord(string keyword, vector<string> words) {
-    int appearedTimes = 0;
-    int totalChars = totalChar(words);
+void keyWord(string keyword, vector<string> words) {// setting up to looking for the key ward int the vector which already create with words
+    int appearedTimes = 0;// show how many times it has been appear in the file
+    int totalChars = totalChar(words);// the total char in the file
     vector<int> appearedCharLocations;
     vector<int> appearedWordLocations;
     int appearedCharLocation = 0;
-    for (int i = 0; i < words.size(); i++) {
+    for (int i = 0; i < words.size(); i++) {// for loop find out the number of keyward
         if (words[i] == keyword) {
             appearedTimes ++;
-            appearedCharLocations.push_back(appearedCharLocation);
-            appearedWordLocations.push_back(i);
+            appearedCharLocations.push_back(appearedCharLocation);// everytime keyward appear each time's number i will add to appearedcharlocation
+            appearedWordLocations.push_back(i);//  everytime keyward appear number i will add to appearedwordlocation
         }
-        appearedCharLocation += words[i].size();
+        appearedCharLocation += words[i].size();// each time keyword apprear in the entire char
     }
     if (appearedTimes == 0) {
         cout << "The word " << keyword << " does not appear in the book." << endl;
@@ -131,10 +131,10 @@ void keyWord(string keyword, vector<string> words) {
     for (int i = 0; i < appearedWordLocations.size(); i++) {
         if (appearedWordLocations[i] != 0 &&
             appearedWordLocations[i] != words.size() - 1) {
-            cout << "at " << static_cast<float> (appearedWordLocations[i]) / static_cast<float> (totalChars) * static_cast<float>(100) << "%: " <<
-            words[appearedWordLocations[i] - 1] << " " <<
+            cout << "at " << static_cast<float> (appearedWordLocations[i]) / static_cast<float> (totalChars) * static_cast<float>(100) << "%: " <<// use static_cast covert int to float.()
+            words[appearedWordLocations[i] - 1] << " " << // show the one word before the key word
             words[appearedWordLocations[i]] << " " <<
-            words[appearedWordLocations[i] + 1] << endl;
+            words[appearedWordLocations[i] + 1] << endl; // show the one word behind the key word
         } else if (appearedWordLocations[i] == 0) {
             cout << "at " << static_cast<float> (appearedWordLocations[i]) / static_cast<float> (totalChars) * static_cast<float>(100) << "%: " <<
             words[appearedWordLocations[i]] << " " <<
@@ -149,17 +149,17 @@ void keyWord(string keyword, vector<string> words) {
 }
 
 int main(int argc, const char * argv[]) {
-    string input = argv[1];
+    string input = argv[1]; // inputting the file from command line
     ifstream fin(input);
     if (fin.fail()) {
         cout << "Failed to find the book!" << endl;
         return 0;
     }
 
-    string word;
-    vector<string> allWords;
+    string word; // create the string "word "
+    vector<string> allWords; // creat the vector "all words"
     while (fin >> word) {
-        allWords.push_back(word);
+        allWords.push_back(word);// push all word to vector allWords
     }
 
     string title = findTitle(allWords);
--- Day12/Status Check 2/Status Check 2/main.cpp
+++ Day12/Status Check 2/Status Check 2/main.cpp
@@ -11,30 +11,40 @@ using namespace std;
 
 
 int main(int argc, const char * argv[]) {
-//    part 2
-//    %clang++ -c my_lib.cpp mani.cpp
-//    %clang++ -o myProg my_lib.o main.o
-//    part 3 
-//    What is the difference between: An array and a structures:
+////    part 2
+////    %clang++ -c my_lib.cpp mani.cpp
+////    %clang++ -o myProg my_lib.o main.o
+////    part 3
+////    What is the difference between: An array and a structures:
+////
+////    A structure creates a data type that can be used to group items of possibly different types into a single type. Array refers to a collection consisting of elements of homogeneous data type. Structure refers to a collection consisting of elements of heterogeneous data type.
+////
+////    What is the difference between: An array and a vector:
+////    Vector is a sequential container to store elements and not index based. Array stores a fixed-size sequential collection of elements of the same type, and it is index based. Vector is dynamic in nature so, size increases with insertion of elements. As array is fixed size, once initialized can't be resized
 //
-//    A structure creates a data type that can be used to group items of possibly different types into a single type. Array refers to a collection consisting of elements of homogeneous data type. Structure refers to a collection consisting of elements of heterogeneous data type.
+//    struct dog {
+//    string  name;
+//    int age;
+//    bool vaccinated;
+//    };
 //
-//    What is the difference between: An array and a vector:
-//    Vector is a sequential container to store elements and not index based. Array stores a fixed-size sequential collection of elements of the same type, and it is index based. Vector is dynamic in nature so, size increases with insertion of elements. As array is fixed size, once initialized can't be resized
-
-    struct dog {
-    string  name;
-    int age;
-    bool vaccinat;
-    };
-
-    struct dogs{
-    vector<dog> dogs;
-    int max_dogs = 12;
-    };
-
+//    dog mydog;
+//    mydog.name = "beef";
+//    mydog.age = 2;
+//    mydog.vaccinated = true;
+//
+//    vector<dog> dogs;
+//
+//
+    
+    int sum(const std::vector<int>& v){
+         int s = 0
+         for[int i=1; i<=v.size(); i++]{
+             s+=v[i];
+         }
+    }
     return 0;
     
-    
+
     
 }
