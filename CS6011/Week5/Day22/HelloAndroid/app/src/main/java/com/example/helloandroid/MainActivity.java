00297795cbc58c0a4a14a9d57606c304f2c63190
junming212900 <junming.work@gmail.com>
2022-11-04 07:26:50 -0600

    update

--- CS6011/Week5/Day22/HelloAndroid/app/src/main/java/com/example/helloandroid/MainActivity.java
+++ CS6011/Week5/Day22/HelloAndroid/app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.helloandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+private boolean firstclick = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void handleClick( View view){
+        Log.i("JJ:MainActivity","button was pressed");
+        if(firstclick) {
+            TextView roomNameTV = findViewById(R.id.RoomNameTxt);
+            roomNameTV.setText("Enter a Room Name");
+            firstclick = false;
+        }
+        else{
+            Intent intent = new Intent(this,AnotherActivity.class);
+startActivity(intent);
+        }
+
     }
 }
